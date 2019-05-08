a90ece4226d233ead32058b762337a904b5497fc
Renato Rotenberg <renato.rotenberg@gmail.com>
2019-05-08 18:38:24 -0300

    Adicionado restart com "R" e estágio 5 melhorado

--- Main.gd
+++ Main.gd
@@ -1,7 +1,10 @@
 extends Spatial
 
 var ball_scene = preload("res://Ball/Ball.tscn")
-var level_scene
+var level
+var ball
+
+var in_level = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 #	var level = level_scene.instance()
@@ -20,10 +23,19 @@ func _ready():
 
 func _on_level_selected(packed_level):
 	$StageSelector.hide()
-	var level = packed_level.instance()
+	level = packed_level.instance()
 	add_child(level)
 
-	var ball := ball_scene.instance() as Ball
+	ball = ball_scene.instance() as Ball
 	ball.global_transform = level.get_node("start").global_transform
 	# ball.global_translate(Vector3(0, 5, 0))
 	add_child(ball)
+	in_level = true
+
+func _unhandled_input(event):
+	if event is InputEventKey:
+		if event.pressed and event.scancode == KEY_R and in_level:
+			in_level = false
+			remove_child(level)
+			remove_child(ball)
+			$StageSelector.show()
