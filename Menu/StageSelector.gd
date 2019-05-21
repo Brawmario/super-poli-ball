61eda0026db147c6ad13b71b19cc710dd300f7c4
Rafael Barone <rafael.barone@qulture.rocks>
2019-05-21 20:34:52 -0300

    Add level 6

--- Menu/StageSelector.gd
+++ Menu/StageSelector.gd
@@ -28,3 +28,8 @@ func _on_ButStage4_pressed():
 func _on_ButStage5_pressed():
 	var level_scene = preload("res://Stages/Stage5/Stage5.tscn")
 	emit_signal("level_selected", level_scene)
+
+
+func _on_ButStage6_pressed():
+	var level_scene = preload("res://Stages/Stage6/Stage6.tscn")
+	emit_signal("level_selected", level_scene)
