ca74e34fd067ea7ed4a12196437bb3f87d32a916
aluno <aluno@labsoft.local>
2019-05-28 19:19:06 -0300

    suporte a controle analógico adicionado

--- Ball/Ball.gd
+++ Ball/Ball.gd
@@ -19,17 +19,13 @@ func _physics_process(delta) -> void:
 	var cam_xform := cam_ball.get_global_transform() as Transform
 
 	# Pegar direção que o jogador quer andar para em relação a camera
-	if (Input.is_action_pressed("ui_up")):
-		dir += -cam_xform.basis[2]
-	if (Input.is_action_pressed("ui_down")):
-		dir += cam_xform.basis[2]
-	if (Input.is_action_pressed("ui_left")):
-		dir += -cam_xform.basis[0]
-	if (Input.is_action_pressed("ui_right")):
-		dir += cam_xform.basis[0]
+	dir += -cam_xform.basis[2] * Input.get_action_strength("move_up")
+	dir +=  cam_xform.basis[2] * Input.get_action_strength("move_down")
+	dir += -cam_xform.basis[0] * Input.get_action_strength("move_left")
+	dir +=  cam_xform.basis[0] * Input.get_action_strength("move_right")
 
 	# Achar vetor de direção com eixo Y igual a zero (no plano XZ)
-	var target_dir := (dir - Vector3.UP*dir.dot(Vector3.UP)).normalized()
+	var target_dir := (dir - Vector3.UP*dir.dot(Vector3.UP))
 
 	# Achar vetor no plano XZ à +90 graus da direção que se deseja ir para
 	# var target_axis := Vector3.UP.cross(target_dir).normalized()
